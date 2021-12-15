c038f4ee10122d7f9452f48e9ef6ea1ae3372afa
Daniel Ames <76114734+satsuk81@users.noreply.github.com>
2021-12-15 17:46:51 +0000

    Add files via upload

--- zscaler-reinstall.ps1
+++ zscaler-reinstall.ps1
@@ -1,9 +1,8 @@
 ﻿$dir = "c:\temp"
 cd $dir
 
-#Set-ExecutionPolicy Bypass -Scope Process -Force
+Set-ExecutionPolicy Bypass -Scope Process -Force
 #[System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072
-#Invoke-Expression ()
 
 $filename = "zscaler-test.msi"
 $file = (New-Object System.Net.WebClient).DownloadString("https://d32a6ru7mhaq0c.cloudfront.net/Zscaler-windows-3.6.1.17-installer.msi")
@@ -12,3 +11,5 @@ $file | Set-Content $filename -Force
 
 msiexec.exe /x "{6823885A-645C-48A4-9410-DC104080F0E6}" /qn
 msiexec.exe /i $filename /qb
+
+#Invoke-Expression ((New-Object System.Net.WebClient).DownloadString("https://raw.githubusercontent.com/satsuk81/test/main/zscaler-reinstall.ps1"))
