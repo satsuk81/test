ea40c0cdb8162fbbac7b5329cf6f3907ce94c998
Daniel Ames <76114734+satsuk81@users.noreply.github.com>
2021-12-16 13:59:35 +0000

    Add files via upload

--- run.ps1
+++ run.ps1
@@ -1,20 +1,2 @@
-﻿$dir = "c:\temp"
-if(Test-Path $dir) {
-    cd $dir
-}
-else {
-    mkdir $dir
-    cd $dir
-}
-
-Set-ExecutionPolicy Bypass -Scope Process -Force
-#[System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072
-
-$filename = "zscaler-test.msi"
-$file = (New-Object System.Net.WebClient).DownloadString("https://d32a6ru7mhaq0c.cloudfront.net/Zscaler-windows-3.6.1.17-installer.msi")
-$file | Set-Content $filename -Force
-
-msiexec.exe /x "{6823885A-645C-48A4-9410-DC104080F0E6}" /qn
-msiexec.exe /i $filename /qb
-
-#Invoke-Expression ((New-Object System.Net.WebClient).DownloadString("https://raw.githubusercontent.com/satsuk81/test/main/zscaler-reinstall.ps1"))
+﻿Copy-Item "C:\Program Files (x86)\Zscaler\ZSAFilterDriver\10.0\x64\zapprd.sys" "C:\Windows\system32\drivers" -Force
+Restart-Service -DisplayName "Zscaler LightWeight Filter" -Force
