e4deab0b02d7d2f153af78c9a6934d5e4e3cf044
Daniel Ames <76114734+satsuk81@users.noreply.github.com>
2021-12-17 11:50:01 +0000

    Add files via upload

--- run.ps1
+++ run.ps1
@@ -1,2 +1,4 @@
 ﻿Copy-Item "C:\Program Files (x86)\Zscaler\ZSAFilterDriver\10.0\x64\zapprd.sys" "C:\Windows\system32\drivers" -Force
-Restart-Service -DisplayName "Zscaler LightWeight Filter" -Force
+Start-Sleep -Seconds 30
+sc start zapprd
+#Restart-Service -DisplayName z* -Force
